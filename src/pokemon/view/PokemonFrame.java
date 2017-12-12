371ee35aa7ef699d5f0d99b4651f6f8615e21d76
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-12 12:59:40 -0700

    Framework for the Pokemon frame.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,21 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import pokemon.controller.PokemonController;
+import javax.swing.JFrame;
 
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController;
+	private PokemonPanel appPanel;
+	
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+	}
 }
