4b7c0dd26207decfd6223a617448daceff8d78aa
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-02 12:44:22 -0700

    Added the framework for the controller class.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,9 +1,28 @@
 package pokemon.controller;
 
+
+import pokemon.model.Pokemon;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PokemonController
 {
-	public void start()
+	private List<Pokemon> pokedex;
+	
+	public List<Pokemon> getPokedex()
 	{
-		
+		return pokedex;
 	}
+	
+	public boolean isValidInteger(String input)
+	{
+		return false;
+	}
+	
+	public boolean isValidDouble(String input)
+	{
+		return false;
+	}
+	
+
 }
