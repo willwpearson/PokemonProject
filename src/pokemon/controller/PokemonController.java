f8b50d4df3b363600bec1f75ed49ffadb5f499df
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-08 11:51:34 -0700

    Updated the getPokemonTypes method.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -8,6 +8,7 @@ public class PokemonController
 {
 	private List<Pokemon> pokedex;
 	
+	
 	public PokemonController()
 	{
 		pokedex = new ArrayList<Pokemon>();	
--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -1,5 +1,8 @@
 package pokemon.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public abstract class Pokemon
 {
 	private int healthPoints;
@@ -17,17 +20,34 @@ public abstract class Pokemon
 	
 	public final String[] getPokemonTypes()
 	{
-		Class<?> [] types = getClass().getInterfaces();
-		String [] pokeTypes = new String[types.length];
+		String [] types = null;
+		ArrayList<String> parentType = new ArrayList<String>();
+		Class<?> currentClass = this.getClass();
 		
-		for(int index = 0; index < types.length; index++)
+		while(currentClass.getSuperclass() != null)
 		{
-			String currentInterface = types[index].getCanonicalName();
-			currentInterface = currentInterface.replace(this.getClass().getPackage().getName() + ".", "");
-			pokeTypes[index] = currentInterface;
+			Class<?> [] pokemonTypes = getClass().getInterfaces();
+			types = new String[pokemonTypes.length];
+			
+
+			for(int index = 0; index < types.length; index++)
+			{
+				String currentInterface = pokemonTypes[index].getCanonicalName();
+				currentInterface = currentInterface.replace(this.getClass().getPackage().getName() + ".", "");
+				parentType.add(currentInterface);
+			}
+			
+			currentClass = currentClass.getSuperclass();
 		}
 		
-		return pokeTypes;
+		types = new String [parentType.size()];
+		
+		for(int index = 0; index < parentType.size(); index++)
+		{
+			types[index] = parentType.get(index);
+		}
+		
+		return types;
 	}
 	
 	public String toString()
