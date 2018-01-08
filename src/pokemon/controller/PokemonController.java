a22fb2fe3d0f51646683488bc848eacc9428d2fa
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-08 11:40:57 -0700

    Added all the pokemon to the pokedex in the controller.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,13 +1,30 @@
 package pokemon.controller;
 
-
-import pokemon.model.Pokemon;
+import pokemon.model.*;
 import java.util.List;
 import java.util.ArrayList;
 
 public class PokemonController
 {
 	private List<Pokemon> pokedex;
+	
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();	
+		buildPokedex();
+		
+		
+	}
+	
+	public void buildPokedex()
+	{
+		pokedex.add(new Darkrai());
+		pokedex.add(new Rotom());
+		pokedex.add(new Mew());
+		pokedex.add(new Mewtwo());
+		pokedex.add(new Umbreon());
+		pokedex.add(new HoopaUnbound());
+	}
 	
 	public List<Pokemon> getPokedex()
 	{
