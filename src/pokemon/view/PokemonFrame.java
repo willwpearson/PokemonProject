0f0673d817871f885b4f96754dd1d1d10907de44
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-10 11:47:55 -0700

    Added the code for the frame of the GUI.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -5,5 +5,28 @@ import javax.swing.JFrame;
 
 public class PokemonFrame extends JFrame
 {
+	private PokemonController appController;
+	private PokemonPanel appPanel;
 	
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new PokemonPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("CS1400 Pokedex");
+		this.setResizable(false);
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
+	
+	public PokemonController getPokemonController()
+	{
+		return appController;
+	}
 }
